17829f4fe2dd387bb06eec9537c495e8ae047154
Michael Matus <michael@matus.at>
2023-04-10 22:19:13 +0200

    Add project files.

---
+++ Bev.Instruments.Spex/Spex.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Threading;
+using Bev.IO.Gpib;
+
+namespace Bev.Instruments.Spex
+{
+    public class Spex
+    {
+        #region Site specific values! Modify on use!
+        /**********************************************/
+        /* There is no documented way to obtain the   */
+        /* instrument`s serial number automatically.  */
+        /* This very site specific method infers the  */
+        /* serial number from the GBIP address.       */
+        /**********************************************/
+        private string GetDeviceSerialNumberForBevLab()
+        {
+            switch (DeviceAddress)
+            {
+                case 1:
+                    return "1930";
+                case 3:
+                    return "4867";
+                default:
+                    return "---";
+            }
+        }
+
+        private string GetInstrumentTypeForBevLab()
+        {
+            switch (DeviceAddress)
+            {
+                case 1:
+                    return "1680 B";
+                case 3:
+                    return "1681 B";
+                default:
+                    return "---";
+            }
+        }
+        #endregion
+
+        public Spex(int deviceAddress, IGpibHandler gpibHandler)
+        {
+            DeviceAddress = deviceAddress;
+            GpibHandler = gpibHandler;
+            wlConv = new WavelengthConverter();
+            Initialize();
+        }
+
+        public IGpibHandler GpibHandler { get; }
+        public int DeviceAddress { get; }
+        public string InstrumentManufacturer => "Jobin-Yvon / SPEX";
+        public string InstrumentType => GetInstrumentType();
+        public string InstrumentSerialNumber => GetDeviceSerialNumber();
+        public string InstrumentFirmwareVersion => GetDeviceFirmwareVersion();
+        public string InstrumentID => $"{InstrumentType} SN:{InstrumentSerialNumber} {InstrumentFirmwareVersion} @ {DeviceAddress:D2}";
+
+        public int GetPositionRaw()
+        {
+            string str = StripFirstChar(OutputEnter("H0"));
+            return int.TryParse(str, out int value) ? value : -1; // good old C++ error return value
+        }
+
+        public void MoveRelativeRaw(int steps)
+        {
+            // TODO check for valid range
+            if (steps == 0) return;
+            if (steps > 0)
+            {
+                MoveRelativeGeneric(steps);
+                ReturnOnHalt();
+            }
+            if (steps < 0)
+            {
+                MoveRelativeGeneric(steps - backlashSteps);
+                ReturnOnHalt();
+                MoveRelativeGeneric(backlashSteps);
+                ReturnOnHalt();
+            }
+        }
+
+        public void MoveAbsoluteRaw(int position)
+        {
+            int currentPos = GetPositionRaw();
+            int steps = position - currentPos;
+            MoveRelative(steps);
+        }
+
+        public double GetPosition() => wlConv.StepsToWavelength(GetPositionRaw());
+
+        public void MoveRelative(double wavelength) => MoveRelativeRaw(wlConv.WavelengthToSteps(wavelength));
+
+        public void MoveAbsolute(double wavelength) => MoveAbsoluteRaw(wlConv.WavelengthToSteps(wavelength));
+
+        private string GetInstrumentType()
+        {
+            // there is no documented way to obtain the type
+            // use a hard coded dicitonary
+            return GetInstrumentTypeForBevLab();
+        }
+
+        private string GetDeviceSerialNumber()
+        {
+            // there is no documented way to obtain the serial number
+            // use a hard coded dicitonary
+            return GetDeviceSerialNumberForBevLab();
+        }
+
+        private string GetDeviceFirmwareVersion()
+        {
+            string zStr = StripFirstChar(OutputEnter("z"));
+            string yStr = StripFirstChar(OutputEnter("y"));
+            return $"{zStr} - {yStr}";
+        }
+
+        private void MoveRelativeGeneric(int steps) => OutputEnter($"F0,{steps}");
+
+        private void ReturnOnHalt()
+        {
+            while (IsBusy()) { }
+        }
+
+        private bool IsBusy()
+        {
+            string str = OutputEnter("E");
+            if (str.Contains("q")) return true;
+            return false;
+        }
+
+        private void Initialize()
+        {
+            GpibHandler.Remote(DeviceAddress);
+            OutputEnter(" ", 200);
+            StartMainProgram();
+        }
+
+        private void StartMainProgram()
+        {
+            char[] buff = { 'O', '0', '0', '0', (char)0 };
+            OutputEnter(new string(buff), 500);
+        }
+
+        private string OutputEnter(string command, int delay)
+        {
+            GpibHandler.Output(DeviceAddress, command);
+            Thread.Sleep(delay);
+            return GpibHandler.Enter(DeviceAddress);
+        }
+
+        private string OutputEnter(string command) => OutputEnter(command, noDelay);
+
+        private string StripFirstChar(string str)
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+            if (str.Length == 1) return str;
+            return str.Substring(1);
+        }
+
+        private WavelengthConverter wlConv;
+        private const int noDelay = 0;
+        private const int defaultDelay = 100;
+        private const int backlashSteps = 500;
+    }
+}
---
+++ Bev.Instruments.Spex/WavelengthConverter.cs
@@ -0,0 +1,20 @@
+﻿namespace Bev.Instruments.Spex
+{
+    public class WavelengthConverter
+    {
+        public WavelengthConverter() : this(50.0, 0.0) { }
+        public WavelengthConverter(double stepsPerNm) : this(stepsPerNm, 0.0) { }
+        public WavelengthConverter(double stepsPerNm, double offset)
+        {
+            StepsPerNanometer = stepsPerNm;
+            StepsOffset = offset;
+        }
+
+        public double StepsPerNanometer { get; }
+        public double StepsOffset { get; }
+
+        public double StepsToWavelength(int steps) => (double)steps / StepsPerNanometer;
+
+        public int WavelengthToSteps(double wavelength) => (int)(wavelength * StepsPerNanometer);
+    }
+}
