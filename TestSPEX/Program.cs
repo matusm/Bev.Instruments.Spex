cdc1067a99e704d26548729be17a47bba5ec4763
Michael Matus <michael@matus.at>
2023-04-13 19:56:48 +0200

    Implement serial IO

---
+++ TestSPEX/Program.cs
@@ -0,0 +1,30 @@
+﻿using Bev.IO.RemoteInterface;
+using Bev.IO.Gpib.Keithley500Serial;
+using Bev.Instruments.Spex;
+using RS232Interface;
+using System;
+
+namespace TestSPEX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comPort = "COM1";
+            int gpibAddress = 3;
+
+            SerialInterface remote = new SerialInterface(comPort);
+            // Keithley500Serial remote = new Keithley500Serial(comPort);
+
+            Spex spex = new Spex(gpibAddress, remote);
+
+            Console.WriteLine(spex.InstrumentID);
+
+            spex.MoveRelativeRaw(1000);
+            Console.WriteLine(spex.GetPositionRaw());
+            spex.MoveRelativeRaw(-1000);
+            Console.WriteLine(spex.GetPositionRaw());
+
+        }
+    }
+}
